1a7d5be2648688425109974bc3da46c45e3f8b35
sumansharma01 <suman.sharma0331@gmail.com>
2021-03-21 18:08:55 +0530

    initialized

---
+++ src/main/java/com/suman/postme/PostmeApplication.java
@@ -0,0 +1,13 @@
+package com.suman.postme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suman/postme/PostmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suman.postme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
